24bc600a1cdd5b3bef7845021288b1ef1e75174f
Daniel Kroening <dkr@amazon.com>
2022-04-14 17:54:51 -0400

    extract substitute_symbols(...) into separate file
    1) The substitute_symbols_rec(...) function in std_expr.cpp is extracted
    into a separate file and is made available to external users as
    substitute_symbols(...).
    
    2) substitute_symbols is now aware of array_comprehension expressions.

--- src/util/std_expr.h
+++ src/util/std_expr.h
@@ -3090,7 +3090,8 @@ inline void validate_expr(const binding_exprt &binding_expr)
 inline const binding_exprt &to_binding_expr(const exprt &expr)
 {
   PRECONDITION(
-    expr.id() == ID_forall || expr.id() == ID_exists || expr.id() == ID_lambda);
+    expr.id() == ID_array_comprehension || expr.id() == ID_forall ||
+    expr.id() == ID_exists || expr.id() == ID_lambda);
   const binding_exprt &ret = static_cast<const binding_exprt &>(expr);
   validate_expr(ret);
   return ret;
